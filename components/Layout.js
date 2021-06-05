84557e06aa747d68f01231a112236183b3b0df9f
Justin Horth <jdhorth@gmail.com>
2021-06-05 19:51:15 -0400

    icons

--- components/Layout.js
+++ components/Layout.js
@@ -19,8 +19,16 @@ export default function Layout({ children }) {
         {children}
       </div>
 
-      <footer>
-        <p>Copyright 2021 fun-bay.com :)</p>
+      <footer><Link href="/">
+        <a><p>Copyright 2021 fun-bay.com :)</p></a>
+      </Link>
+        <div class="social">
+          <a href="https://twitter.com/jdhorth" target="_blank"><i class="fab fa-twitter fa-3x"></i></a>
+          <a href="https://facebook.com/justinhorth" target="_blank"><i class="fab fa-facebook fa-3x"></i></a>
+          <a href="https://youtube.com/jdubhman" target="_blank"><i class="fab fa-youtube fa-3x"></i></a>
+          <a href="https://www.linkedin.com/in/justinhorth" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
+          <a href="https://github.com/jdhorth" target="_blank"><i class="fab fa-github fa-3x"></i></a>
+        </div>
       </footer>
     </div>
   )
