56f0420a846b86da4c9ae73610f63b8f31ef3a30
Justin Horth <jdhorth@gmail.com>
2021-06-05 01:08:20 -0400

    initial

--- components/Layout.js
+++ components/Layout.js
@@ -7,8 +7,8 @@ export default function Layout({ children }) {
         <Link href="/">
           <a>
             <h1>
-              <span>Just Add</span>
-              <span>Marmite</span>
+              <span>All You Gotta Do</span>
+              <span>is Add Marmite</span>
             </h1>
             <h2>Spread The Joy</h2>
           </a>
@@ -16,11 +16,11 @@ export default function Layout({ children }) {
       </header>
 
       <div className="page-content">
-        { children }
+        {children}
       </div>
 
       <footer>
-        <p>Copyright 2021 Just Add Marmite :)</p>
+        <p>Copyright 2021 Add Marmite :)</p>
       </footer>
     </div>
   )
